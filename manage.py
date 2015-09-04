569135228d39c7086648562d4a3167df5a4951dd
Nick Caramello <nick@podconsulting.gom>
2015-09-04 15:30:44 -0400

    GEM:10460 trying again

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 import os
 import sys
 
+
 if __name__ == "__main__":
     os.environ["DJANGO_SETTINGS_MODULE"] = "MediasiteProvisioning.settings.local"
     # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MediasiteProvisioning.settings.local")
