36bbe2bc46947884098324e64dbf060df70b2894
Nick Caramello <nick@podconsulting.gom>
2015-11-26 06:21:57 -0500

    updated the project to work for local development

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     if 'test' in sys.argv:
         os.environ['DJANGO_SETTINGS_MODULE'] = 'mediasite_provisioning.settings.test'
     else:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mediasite_provisioning.settings.aws')
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mediasite_provisioning.settings.local')
     # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MediasiteProvisioning.settings.local")
 
     from django.core.management import execute_from_command_line
