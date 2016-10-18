ce1984c81dd78f91feee1b10c6608cb948a47c6a
Yates, Elliott <elliott_yates@harvard.edu>
2016-10-18 10:12:11 -0400

    tlt-2859: search form - account selection restricted to valid accounts - Canvas account list dropdown on search form now displays a de-duplicated list of accounts for which the web.models.School has all the required information to provision (ie canvas ID, root folder, consumer key, and secret)

--- web/forms.py
+++ web/forms.py
@@ -1,37 +1,46 @@
 from django import forms
 from django.core.exceptions import ObjectDoesNotExist
-from canvas.apimethods import CanvasAPI
+
 from .models import School
+from canvas.apimethods import CanvasAPI
+
 
 class IndexForm(forms.Form):
     accounts = None
-    search = forms.CharField(min_length=3, widget=forms.TextInput(attrs={'placeholder': 'Please enter at least 3 characters'}))
+    search = forms.CharField(min_length=3, widget=forms.TextInput(
+        attrs={'placeholder': 'Please enter at least 3 characters'}))
     search_results = ()
 
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop('user')
         super(IndexForm, self).__init__(*args, **kwargs)
 
-        account_choices = list()
+        account_choices = set()
         canvas_api = CanvasAPI(user=self.user)
         accounts = canvas_api.get_accounts_for_current_user()
         if accounts:
             for account in accounts:
-                account_choice = ((account.id, account.name))
-                account_choices.append(account_choice)
-                # we also save/update account information
-                school = School(canvas_id=account.id, name=account.name)
                 try:
-                    school = School.objects.get(canvas_id = account.id)
+                    school = School.objects.get(canvas_id=account.id)
+                    # update school name in case it has changed in Canvas
                     school.name = account.name
+                    school.save()
+                    if self._is_valid_school_account(school):
+                        account_choice = (account.id, account.name)
+                        account_choices.add(account_choice)
                 except ObjectDoesNotExist:
                     # do nothing since we initialized school above
                     pass
-                school.save()
 
-        self.fields['accounts'] = forms.ChoiceField(choices=account_choices, required=True)
+        self.fields['accounts'] = forms.ChoiceField(choices=account_choices,
+                                                    required=True)
 
-
-
-
-
+    @staticmethod
+    def _is_valid_school_account(school):
+        # a school is a valid choice in the form dropdown if it has all the
+        # information required to reliably provision a lecture video Canvas
+        # integration
+        return school.canvas_id and \
+               school.mediasite_root_folder and \
+               school.consumer_key and \
+               school.shared_secret
