0e58eb4863819ed242d6eb6d2e3d4efc57e1db74
Yates, Elliott <elliott_yates@harvard.edu>
2016-10-19 11:54:58 -0400

    tlt-2859: search form - account list updates - if a user is staff, show all schools (that we have enough information to provision) - regular users see all (provisionable) schools they have account membership for in Canvas - account list is sorted

--- web/forms.py
+++ web/forms.py
@@ -16,39 +16,28 @@ class IndexForm(forms.Form):
         super(IndexForm, self).__init__(*args, **kwargs)
 
         account_choices = set()
-        canvas_api = CanvasAPI(user=self.user)
-        accounts = canvas_api.get_accounts_for_current_user()
-        if accounts:
-            if [a for a in accounts if a.id == 1]:
-                # user has a root account permission, allow all schools in
-                # dropdown
-                schools = School.objects.all()
-                valid_schools = [(s.canvas_id, s.name) for s in schools
-                                 if self._is_valid_school_account(s)]
-                account_choices = set(valid_schools)
-            else:
-                for account in accounts:
-                    try:
-                        school = School.objects.get(canvas_id=account.id)
-                        # update school name in case it has changed in Canvas
-                        school.name = account.name
-                        school.save()
-                        if self._is_valid_school_account(school):
-                            account_choice = (account.id, account.name)
-                            account_choices.add(account_choice)
-                    except ObjectDoesNotExist:
-                        # do nothing since we initialized school above
-                        pass
+        if self.user.is_staff:
+            # user should be able to provision for any school
+            schools = School.objects.all()
+            valid_schools = [(s.canvas_id, s.name) for s in schools
+                             if s.can_be_provisioned]
+            account_choices = set(valid_schools)
+        else:
+            canvas_api = CanvasAPI(user=self.user)
+            accounts = canvas_api.get_accounts_for_current_user()
+            for account in accounts:
+                try:
+                    school = School.objects.get(canvas_id=account.id)
+                    # update school name in case it has changed in Canvas
+                    school.name = account.name
+                    school.save()
+                    if school.can_be_provisioned:
+                        account_choice = (account.id, account.name)
+                        account_choices.add(account_choice)
+                except ObjectDoesNotExist:
+                    # do nothing since we initialized school above
+                    pass
 
-        self.fields['accounts'] = forms.ChoiceField(choices=account_choices,
+        sorted_choices = sorted(account_choices, key=lambda a: a[1])
+        self.fields['accounts'] = forms.ChoiceField(choices=sorted_choices,
                                                     required=True)
-
-    @staticmethod
-    def _is_valid_school_account(school):
-        # A school is a valid choice in the form dropdown if it has all the
-        # information required to reliably provision a lecture video Canvas
-        # integration (consumer key and secret can fall back to values in
-        # SETTINGS if needed, but canvas_id and mediasite_root_folder should be
-        # specified for each school to be able to query canvas and create the
-        # mediasite folder structure).
-        return school.canvas_id and school.mediasite_root_folder
--- web/models.py
+++ web/models.py
@@ -1,5 +1,6 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+
 
 class School(models.Model):
     canvas_id = models.TextField()
@@ -11,9 +12,21 @@ class School(models.Model):
     catalog_show_time = models.BooleanField(default=False, null=False)
     catalog_items_per_page = models.IntegerField(blank=100, null=100)
 
+    @property
+    def can_be_provisioned(self):
+        # A school is a valid choice in the form dropdown if it has all the
+        # information required to reliably provision a lecture video Canvas
+        # integration (consumer key and secret can fall back to values in
+        # SETTINGS if needed, but canvas_id and mediasite_root_folder should be
+        # specified for each school to be able to query canvas and create the
+        # mediasite folder structure).
+        return bool(self.canvas_id) and bool(self.mediasite_root_folder)
+
+
 class APIUser(models.Model):
     user = models.OneToOneField(User)
     canvas_api_key = models.TextField()
+
 
 class Log(models.Model):
     username = models.TextField(blank=True, null=True)
