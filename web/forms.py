bc61d49f7a35f091f45efe1180e07330c6166568
Yates, Elliott <elliott_yates@harvard.edu>
2016-10-19 10:10:55 -0400

    tlt-2859: search form - account list updates - if user has root account permissions, show all schools - valid list of schools now includes those without explicit consumer key and secret overrides; these will fall back on the default mediasite lti profile as defined by the consumer key

--- web/forms.py
+++ web/forms.py
@@ -19,28 +19,36 @@ class IndexForm(forms.Form):
         canvas_api = CanvasAPI(user=self.user)
         accounts = canvas_api.get_accounts_for_current_user()
         if accounts:
-            for account in accounts:
-                try:
-                    school = School.objects.get(canvas_id=account.id)
-                    # update school name in case it has changed in Canvas
-                    school.name = account.name
-                    school.save()
-                    if self._is_valid_school_account(school):
-                        account_choice = (account.id, account.name)
-                        account_choices.add(account_choice)
-                except ObjectDoesNotExist:
-                    # do nothing since we initialized school above
-                    pass
+            if [a for a in accounts if a.id == 1]:
+                # user has a root account permission, allow all schools in
+                # dropdown
+                schools = School.objects.all()
+                valid_schools = [(s.canvas_id, s.name) for s in schools
+                                 if self._is_valid_school_account(s)]
+                account_choices = set(valid_schools)
+            else:
+                for account in accounts:
+                    try:
+                        school = School.objects.get(canvas_id=account.id)
+                        # update school name in case it has changed in Canvas
+                        school.name = account.name
+                        school.save()
+                        if self._is_valid_school_account(school):
+                            account_choice = (account.id, account.name)
+                            account_choices.add(account_choice)
+                    except ObjectDoesNotExist:
+                        # do nothing since we initialized school above
+                        pass
 
         self.fields['accounts'] = forms.ChoiceField(choices=account_choices,
                                                     required=True)
 
     @staticmethod
     def _is_valid_school_account(school):
-        # a school is a valid choice in the form dropdown if it has all the
+        # A school is a valid choice in the form dropdown if it has all the
         # information required to reliably provision a lecture video Canvas
-        # integration
-        return school.canvas_id and \
-               school.mediasite_root_folder and \
-               school.consumer_key and \
-               school.shared_secret
+        # integration (consumer key and secret can fall back to values in
+        # SETTINGS if needed, but canvas_id and mediasite_root_folder should be
+        # specified for each school to be able to query canvas and create the
+        # mediasite folder structure).
+        return school.canvas_id and school.mediasite_root_folder
